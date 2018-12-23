2cfae66670b98c643c4a09073c5e77b22ab6e793
Michael Torres <michaelj.torres@yahoo.com>
2018-12-22 20:57:35 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+//                           #1 First and Last Character
+
+/*
+function removeChar(str){
+return str.slice(1, -1);
+};
+*/
+
+/*                      #2 Basic Mathematical Operations
+
+function basicOp(operation, value1, value2)
+{
+  if(operation === "+"){
+   return value1 + value2;
+  } else if (operation === "-"){
+  return value1 - value2;
+  } else if (operation === "*"){
+   return value1 * value2;
+  } else if (operation === "/"){
+  return value1 / value2
+  }
+
+console.log("*",2,4);
+}
+*/
+
+/*                    #3  Volume of a Cuboid
+
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    return length * width * height;
+  };
+
+  return Kata;
+
+})();
+
+*/
+
+/*               #4 Grasshopper - Personalized Message
+
+function greet (name, owner) {
+   return name == owner ? 'Hello boss' : 'Hello guest';
+}
+
+greet('Mike','dude')
+
+*/
+
+/*                 #5 Even or Odd
+
+function even_or_odd(number) {
+ if(number % 2 === 0) {
+     return "Even";
+ } else {
+    return "Odd"
+ }
+}
+
+/*                #6  Function 2 - squaring an argument
+
+function square(input){
+  return input * input;
+};
+
+*/
+
+/*       #7 Multiply
+
+function multiply(a, b){
+ return a * b
+}
+*/
+
+/*       #8 Find the smallest integer in the array
+           function numberToString(num) {
+           return num.toString();
+                      }
+                    */
+/*        #9 Opposite number
+function opposite(number) {
+return -number
+}
+*/
+
+const boo = (num1, num2) => {
+   return num1 + num2
+}
+
+console.log(boo(2,3));
